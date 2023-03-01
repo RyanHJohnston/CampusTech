e94f092d5cb639c1300203f79fd020c941e9a5c3
Ryan Johnston <ryanhjohnstoncollege@gmail.com>
2023-03-01 13:49:09 -0600

    Added Bootstrap CSS, jQuery, and Popper.js. These frameworks should help with stylizing the frontend.

--- src/main/java/com/example/softengproject/Controller/DemoController.java
+++ src/main/java/com/example/softengproject/Controller/DemoController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 /* Initiates thymeleaf template (index.html) 
  * This is where we will manipulate the data in the frontend
  * We can modify the name of the controller class later
