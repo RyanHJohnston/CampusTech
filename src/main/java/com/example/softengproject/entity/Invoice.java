612cf03d53a05050285e80c071bb9c0a6b1caa4b
Ryan Johnston <ryanhjohnstoncollege@gmail.com>
2023-04-04 17:45:41 -0500

    Commit message test

--- src/main/java/com/example/softengproject/entity/Invoice.java
+++ src/main/java/com/example/softengproject/entity/Invoice.java
@@ -1,10 +1,16 @@
 package com.example.softengproject.entity;
 
 import java.util.*;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.lang.*;
 import java.sql.Date;
 
+@Entity
+@Data
 public class Invoice implements Serializable {
     
     private Long id;
